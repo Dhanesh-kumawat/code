4d2a9784bd6ea770b7a6e1faad23253f6f8ab9be
Dhanesh-kumawat <dhaneshkumawat236@gmail.com>
2026-05-22 16:25:40 +0530

    add new inital files

---
+++ code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <deque>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Stack{
+  public:
+    vector<int> v;
+
+    public:
+     
+    void push(int val){
+      v.push_back(val);
+    }
+
+    void pop(){
+      v.pop_back();
+    }
+
+    int top(){
+      return v[v.size()-1];
+    }
+
+    bool empty(){
+      return v.size() ==0;
+    }
+};
+
+int main(){
+  Stack s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+
+  while(!s.empty() ){
+    cout<<s.top()<<endl;
+    s.pop();
+  }
+
+  return 0;
+
+}
