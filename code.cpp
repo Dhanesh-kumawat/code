48d651def11789e99c0003adbcfd4f062a0efbcc
Dhanesh-kumawat <dhaneshkumawat236@gmail.com>
2026-06-01 13:04:14 +0530

    Stock Spam problem solution DSA(chapter-70)

--- code.cpp
+++ code.cpp
@@ -8,32 +8,31 @@
 
 using namespace std;
 
-bool isvlide(string str){
-
-  stack<char>st;
-
-  for(int i=0;i<str.size();i++){
-    if(str[i] =='(' || str[i] == '{' || str[i] == '['){ //opening
-      st.push(str[i]);
-    }else{
-      if(st.size()==0){ //machting the string
-        return false;
-      }if((st.top() == '(' && str[i]==')') ||
-            (st.top()=='{' && str[i] =='}') ||
-          (st.top() =='[' && str[i]==']')){
-            st.pop();
-          }else{
-            return false;
-          }
-    }
-  }
-  return st.size() == 0;
-}
 int main(){
-  string str ="([{])";
-  // cout<<"enter the string : ";
-  // cin>>str;
+  vector<int>price= {100,80,60,70,60,75,85};
 
-  cout<<isvlide(str);
+  vector<int>ans(price.size(),0);
+
+  stack<int>s; /// this stack is for store the previous high value from any current stock price:
+
+  for(int i=0;i<price.size();i++){
+    while(s.size() >0 && price[s.top()] <= price[i]){
+      s.pop();
+    }
+
+    if(s.empty()){
+      ans[i] = i+1;
+    }else{
+      ans[i] =i-s.top();// i-prevHigh
+    }
+    s.push(i);
+  }
+  
+  //print ans values
+  for(int val : ans){
+    cout<<val<<" ";
+  }
+  cout<<endl;
+
   return 0;
 }
