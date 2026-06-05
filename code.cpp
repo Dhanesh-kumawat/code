49a1831832494d2f6e034d0e0d20125398299166
Dhanesh-kumawat <dhaneshkumawat236@gmail.com>
2026-06-05 15:14:34 +0530

    solution of Next Greater Element problem

--- code.cpp
+++ code.cpp
@@ -9,30 +9,28 @@
 using namespace std;
 
 int main(){
-  vector<int>price= {100,80,60,70,60,75,85};
+  vector<int>arr = {6,8,0,1,3};
 
-  vector<int>ans(price.size(),0);
+  stack<int>s;
+  vector<int>ans(arr.size(),0);
 
-  stack<int>s; /// this stack is for store the previous high value from any current stock price:
-
-  for(int i=0;i<price.size();i++){
-    while(s.size() >0 && price[s.top()] <= price[i]){
+  for(int i = arr.size()-1;i>=0;i--){
+    while(s.size()>0 && s.top() <= arr[i]){
       s.pop();
     }
-
     if(s.empty()){
-      ans[i] = i+1;
+      ans[i] =-1;
     }else{
-      ans[i] =i-s.top();// i-prevHigh
+       ans[i]=s.top();
     }
-    s.push(i);
+    s.push(arr[i]);
   }
-  
-  //print ans values
+
+  // print the Next Greater element 
   for(int val : ans){
     cout<<val<<" ";
   }
   cout<<endl;
-
-  return 0;
+ 
+  return 0; 
 }
