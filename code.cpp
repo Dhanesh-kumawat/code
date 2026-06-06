7be7f1bfc0f4d09f7ada5878da2b4a99c255cead
Dhanesh-kumawat <dhaneshkumawat236@gmail.com>
2026-06-06 14:14:01 +0530

    solution of previous smaller element

--- code.cpp
+++ code.cpp
@@ -9,28 +9,29 @@
 using namespace std;
 
 int main(){
-  vector<int>arr = {6,8,0,1,3};
+  vector<int>arr = {3,1,0,8,6};
 
   stack<int>s;
   vector<int>ans(arr.size(),0);
 
-  for(int i = arr.size()-1;i>=0;i--){
-    while(s.size()>0 && s.top() <= arr[i]){
+  for(int i=0; i<arr.size();i++){
+    while(s.size()>0 && s.top() >= arr[i]){
       s.pop();
     }
     if(s.empty()){
-      ans[i] =-1;
+      ans[i]=-1;
     }else{
-       ans[i]=s.top();
+      ans[i]= s.top();
     }
+
     s.push(arr[i]);
   }
 
-  // print the Next Greater element 
-  for(int val : ans){
-    cout<<val<<" ";
-  }
-  cout<<endl;
+ //print the answer
+ for(int val : ans){
+  cout<<val<<" ";
+ }
+ cout<<endl;
  
   return 0; 
 }
